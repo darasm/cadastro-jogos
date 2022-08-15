cdc1a7793cff721a7b8b1196ea36ef15ea64f67f
darasm <dara_souza.12@hotmail.com>
2022-08-15 07:23:07 -0300

    [DONE] - building mysql container

--- src/main/java/com/dontclosethedoor/game/exception/ExceptionHandler.java
+++ src/main/java/com/dontclosethedoor/game/exception/ExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.dontclosethedoor.game.exception;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
